eae2ccb54695ff6d0bbccf1b3a4af6359553e7d9
InRabeGH <rabe_edgar@hotmail.com>
2021-11-08 21:43:23 -0600

    Tratando de mutear

--- index.js
+++ index.js
@@ -188,8 +188,49 @@ discordClient.on('message', async (msg) => {
                   msg.reply('Ya llegue prros')
           }
       } else if (msg.content.trim().toLowerCase() == _CMD_MUTE) {                 //COMANDO MUTE
-        voice_Connection.on('Hablando', async (user, speaking) => {
-
+        voice_Connection.on('Muteando', message => {
+          // Ignore messages that aren't from a guild
+          if (!message.guild) return;
+        
+          // If the message content starts with "!kick"
+          if (message.content.startsWith('|callate')) {
+            // Assuming we mention someone in the message, this will return the user
+            // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+            const user = message.mentions.users.first();
+            // If we have a user mentioned
+            if (user) {
+              // Now we get the member from the user
+              const member = message.guild.member(user);
+              // If the member is in the guild
+              if (member) {
+                /**
+                 * Kick the member
+                 * Make sure you run this on a member, not a user!
+                 * There are big differences between a user and a member
+                 */
+                member
+                  .kick('Me caes de la verga !')
+                  .then(() => {
+                    // We let the message author know we were able to kick the person
+                    message.reply(`Sacamos a la chingada a ${user.tag}`);
+                  })
+                  .catch(err => {
+                    // An error happened
+                    // This is generally due to the bot not being able to kick the member,
+                    // either due to missing permissions or role hierarchy
+                    message.reply('Que pedo, que pedoo!');
+                    // Log the error
+                    console.error(err);
+                  });
+              } else {
+                // The mentioned user isn't in this guild
+                message.reply("Este ni esta loco!");
+              }
+              // Otherwise, if no user was mentioned
+            } else {
+              message.reply("Tienes que poner a quien correr !");
+            }
+          }
         });                                                                       // FIN COMANDO MUTE
       } else if (msg.content.trim().toLowerCase() == _CMD_LEAVE) {
           if (guildMap.has(mapKey)) {
